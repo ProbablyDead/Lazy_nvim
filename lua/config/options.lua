931992ce446daa4ec86e77b53feda39dd5b6cd5d
ProbablyDead <volodin.is.03@mail.ru>
2024-10-13 00:49:29 +0000

    init

---
+++ lua/config/options.lua
@@ -0,0 +1,32 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.opt.guicursor = "a:block"
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.termguicolors = true
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "
+vim.o.background = "light"
+
+vim.env.LANG = 'en_US.UTF-8'
+vim.env.LC_ALL = 'en_US.UTF-8'
