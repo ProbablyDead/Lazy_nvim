a35a7a2d78f13510772f5d9593a60f64ccd8842b
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2025-01-03 02:06:08 +0300

    fckng working

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,10 +1,10 @@
 return {
-    { "bufferline.nvim",        enabled = false },
-    { "nvimdev/dashboard-nvim", enabled = false },
-    { "flash.nvim",             enabled = false },
-    { "which-key.nvim",         enabled = false },
-    { "conform.nvim",           enabled = false },
-    { "noice.nvim",             enabled = false },
-    { "mini.pairs",             enabled = false },
-    { "persistence.nvim",       enabled = false },
+    { "bufferline.nvim",   enabled = false },
+    { "flash.nvim",        enabled = false },
+    { "which-key.nvim",    enabled = false },
+    { "conform.nvim",      enabled = false },
+    { "mini.pairs",        enabled = false },
+    { "noice.nvim",        enabled = false },
+    { "persistence.nvim",  enabled = false },
+    { "folke/snacks.nvim", opts = { dashboard = { enabled = false } } },
 }
